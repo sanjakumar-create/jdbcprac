b84260a59d0b70f26cdcb5f8542ecd718136e3dc
Sanjay Kumar <sanjakumar@griddynamics.com>
2026-03-03 12:52:29 +0530

    new changes

--- src/demoJdbc.java
+++ src/demoJdbc.java
@@ -16,17 +16,28 @@ public class demoJdbc {
         String url="jdbc:postgresql://localhost:5433/student";
         String uname="postgres";
         String psd="Kumar@1234";
-        String query="Select stname from info where sid=12";
-
+        //String query="Select stname from info where sid=10";
+         String sql="select * from info";
         //Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url,uname,psd);
         System.out.println("connection estblish");
          Statement stmt = conn.createStatement();
-           ResultSet rs= stmt.executeQuery(query);
-        System.out.println(rs.next());
+          ResultSet rs= stmt.executeQuery(sql);
+        //System.out.println(rs.next());
 
-        String name=rs.getString("stname");
-           System.out.println("our student name is :"+name);
+        //String name=rs.getString("stname");
+           //System.out.println("our student name is :"+name);
+
+        //new tchniwu to print all the data from the table
+        //by using the rs.next that will search for the exting the next data and responsible for moving the pointer
+        System.out.println("name "+"stid "+"stage");
+        while(rs.next()){
+              System.out.print(rs.getString(1)+"-");
+              System.out.print(rs.getInt(2)+"-");
+              System.out.print(rs.getInt(3));
+              System.out.println();
+
+          }
            conn.close();
            System.out.println("connection close");
         
